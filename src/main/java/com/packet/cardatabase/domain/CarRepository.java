95fcce6a1bedf197b459667f0b273fd9d7d19dcf
dd2558 <dd2558@naver.com>
2024-04-25 12:45:49 +0900

    rest 의존성 추가

--- src/main/java/com/packet/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packet/cardatabase/domain/CarRepository.java
@@ -1,15 +1,7 @@
 package com.packet.cardatabase.domain;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
+/*@RepositoryRestResource(path = "vehicles")*/
 public interface CarRepository extends CrudRepository<Car, Long> {
-    @Query("select c from Car c where c.brand like %?1")
-    List<Car> findByBrand(String brand);
-
-    List<Car> findByColor(String model);
-
-    List<Car> findByYear(int year);
 }
--- src/main/java/com/packet/cardatabase/domain/Owner.java
+++ src/main/java/com/packet/cardatabase/domain/Owner.java
@@ -1,5 +1,7 @@
 package com.packet.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.Setter;
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @NoArgsConstructor
 @Getter
 @Setter
@@ -18,6 +21,8 @@ public class Owner {
 
     private String firstname, lastname;
 
+
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
     private List<Car> cars;
 
