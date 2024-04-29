07261d828280d60d41305f87559461fb7bd65e16
dd2558 <dd2558@naver.com>
2024-04-29 17:11:48 +0900

    domain수정

--- src/main/java/com/packet/cardatabase/domain/Car.java
+++ src/main/java/com/packet/cardatabase/domain/Car.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String brand, model, color, registerNumber;
